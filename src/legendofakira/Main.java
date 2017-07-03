ccc1e05c46079c1c85395e3cc30343a8cb41a2d9
Yoshiki Honda <y-honda@ist.osaka-u.ac.jp>
2017-07-03 15:38:48 +0900

    サンプルコードを入れた

--- src/legendofakira/Main.java
+++ src/legendofakira/Main.java
@@ -1,7 +1,48 @@
 package legendofakira;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+
+import gnu.io.CommPortIdentifier;
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
+import gnu.io.SerialPort;
+import gnu.io.UnsupportedCommOperationException;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Start Develop");
+	try
+	  {
+		 CommPortIdentifier portID = CommPortIdentifier.getPortIdentifier("/dev/tty.usbserial-A400FMPJ");
+		   SerialPort port = (SerialPort)portID.open("Sample", 5000); //waiting5000ms
+		       
+		   port.setSerialPortParams(19200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+		   port.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
+		    
+		   InputStream is = port.getInputStream();
+		    
+		   int c;
+		   while((c = is.read()) != -1){
+		      Calendar cal = Calendar.getInstance();
+		      System.out.print(cal.get(Calendar.SECOND) + "." + cal.get(Calendar.MILLISECOND));
+		      System.out.println("->" + (char)c);
+		   }
+		   is.close();
+		   port.close();
+		  }catch(NoSuchPortException e){
+		   System.err.println("Can Not Find Device");
+		   e.printStackTrace();
+		  }catch(PortInUseException e){
+		   System.err.println("Can Not Open Device");
+		   e.printStackTrace();
+		  }catch(UnsupportedCommOperationException e){
+		   System.err.println("Invalid Parameter");
+		   e.printStackTrace();
+		  }catch(IOException e){
+		   e.printStackTrace();
+		  }catch(Exception e){
+		   e.printStackTrace();
+		  }
 	}
 }
